6bbed04f1b3c915fb99afe3b9bf7f5dc3b304197
Ryan Lovelett <ryan@lovelett.me>
2012-08-06 22:07:40 -0500

    Implement database dump to file system

--- lib/wordpress_deploy/database/mysql.rb
+++ lib/wordpress_deploy/database/mysql.rb
@@ -7,14 +7,44 @@ module WordpressDeploy
       include WordpressDeploy::ConfigurationFile
       include WordpressDeploy::Cli::Helpers
 
-      def mysqldump
-        "#{utility("mysqldump")} #{arguments}"
+      ##
+      # The file that the instance would save to if
+      # save is called.
+      def file
+        File.join(Environment.sql_dir, "#{name}.sql")
+      end
+
+      ##
+      # Save the database to a file locally.
+      #
+      # The database will be output into #file.
+      def save!
+        # Get the output from MySQL Dump
+        cmd = mysqldump
+        dump_str = run cmd
+
+        # Open the supplied file; or create a temporary one
+        file_io = File.new(file, 'w')
+
+        # Start writing to file
+        file_io.write(dump_str)
+
+        true
+      ensure
+        file_io.close unless file_io.nil?
       end
 
       private
 
-      def arguments
-        "-P \"#{db_port}\" -h \"#{db_hostname}\" -u \"#{self.DB_USER}\" -p#{self.DB_PASSWORD} -B \"#{self.DB_NAME}\""
+      def mysqldump
+        arg_port = db_port
+        arg_host = db_hostname
+        arg_user = self.DB_USER
+        arg_pass = self.DB_PASSWORD
+        arg_name = self.DB_NAME
+        arguments = "-P \"#{arg_port}\" -h \"#{arg_host}\" -u \"#{arg_user}\" -p\"#{arg_pass}\" -B \"#{arg_name}\""
+
+        "#{utility("mysqldump")} #{arguments}"
       end
 
       ##
