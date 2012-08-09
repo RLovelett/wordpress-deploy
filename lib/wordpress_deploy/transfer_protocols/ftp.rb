85d19904d2ec03460b6744c2a54a22b66ee8c7ca
Ryan Lovelett <ryan@lovelett.me>
2012-08-08 22:30:14 -0500

    Setup FTP for different configurations

--- lib/wordpress_deploy/transfer_protocols/ftp.rb
+++ lib/wordpress_deploy/transfer_protocols/ftp.rb
@@ -8,6 +8,165 @@ require "action_view"
 module WordpressDeploy
   module TransferProtocols
     class Ftp
+      include ActionView::Helpers::NumberHelper
+
+      attr_reader :configuration
+      attr_accessor :available_names
+      alias :names :available_names
+
+      ##
+      # Create a new instance of the Ftp object
+      def initialize(config_name = nil)
+        # Set the configuration
+        @yaml             = YAML.load_file(File.join(Environment.config_dir, "wp-config.yml"))
+        @available_names  = @yaml.map { |key, val| key }
+
+        self.name = config_name unless config_name.nil?
+
+        # Actually open the connection
+        connect
+      end
+
+      def configuration=(new_config)
+        @configuration = new_config if new_config.instance_of? WordpressDeploy::Wordpress::Configuration
+      end
+
+      def transmit
+        raise NotImplementedError
+      end
+
+      def transmit!
+
+      end
+
+      def receive
+        raise NotImplementedError
+      end
+
+      def receive!
+
+      end
+
+      ##
+      # Return the configuration's name.
+      #
+      # Defaults to the first configuration name.
+      def name
+        @name ||= available_names.first
+        @name
+      end
+
+      ##
+      # Set the configuration's name.
+      #
+      # Only performs the assignment if the proposed name is
+      # an available name.
+      #
+      # Returns the Configuration's name.
+      def name=(new_name)
+        @name = new_name if name? new_name
+        @name
+      end
+
+      ##
+      # Test if the name passed in is an available configuration
+      # name.
+      def name?(name_to_check)
+        available_names.include? name_to_check
+      end
+
+      def local_path
+        Environment.wp_dir
+      end
+
+      def remote_path
+        ftp_dir = send(:FTP_DIR)
+        return "/" if ftp_dir.nil?
+        ftp_dir
+      end
+
+      def username
+        send(:FTP_USER) || ""
+      end
+
+      def password
+        send(:FTP_PASSWORD) || ""
+      end
+
+      def port
+        port = 21
+        match = /:(?<port>\d+)$/.match(send(:FTP_HOST))
+        port = match[:port].to_i unless match.nil?
+        port
+      end
+
+      ##
+      # Does FTP_HOST contain a port number?
+      def port?
+        !(send(:FTP_HOST) =~ /:(?<port>\d+)$/).nil?
+      end
+
+      ##
+      # Get just the hostname from DB_HOST. Only different from
+      # FTP_HOST if FTP_HOST has a port number in it.
+      def host
+        host = "localhost"
+        match = /(?<host>.*?)(?=:|$)/.match(send(:FTP_HOST))
+        host = match[:host].to_s unless match.nil?
+        host
+      end
+
+      FTP_CONFIGURATION_ATTRIBUTES = [:FTP_DIR, :FTP_USER, :FTP_PASSWORD,
+                                      :FTP_HOST]
+
+      ##
+      # Define the behaviours of the default parameters quickly
+      def method_missing(meth, *args, &block)
+        # Convert the method to a symbol
+        method_symbol = meth.to_sym
+
+        if FTP_CONFIGURATION_ATTRIBUTES.include? method_symbol
+          config = @yaml[name]
+          return config[meth.to_s] if config.include? meth.to_s
+        else
+          # You *must* call super if you don't handle the method, otherwise
+          # you will mess up Ruby's method lookup.
+          super
+        end
+      end
+
+      ##
+      # Define respond_to?
+      def respond_to?(method)
+        return true if FTP_CONFIGURATION_ATTRIBUTES.include? method.to_sym
+        super
+      end
+
+      private
+
+      def local_path
+        Environment.wp_dir
+      end
+
+      ##
+      # Establish a connection to the remote server
+      def connect
+
+        # If the user defined a port
+        if port?
+          # Unset the FTP port number if it is defined
+          if Net::FTP.const_defined?(:FTP_PORT)
+            Net::FTP.send(:remove_const, :FTP_PORT)
+          end
+
+          # Set it to the user defined or default value
+          Net::FTP.send(:const_set, :FTP_PORT, port)
+        end
+
+        # Now open the connection to the remote machine
+        @ftp = Net::FTP.new(host, username, password)
+
+      end
 
     end
   end
--- spec/transfer_protocols/ftp_spec.rb
+++ spec/transfer_protocols/ftp_spec.rb
@@ -1,5 +1,155 @@
 require 'spec_helper'
 
+include WordpressDeploy
+
 describe WordpressDeploy::TransferProtocols::Ftp do
+  before(:each) do
+    Net::FTP.stub(:new)
+  end
+
+  it { should respond_to :name }
+  it { should respond_to :name= }
+  it { should respond_to :available_names }
+  it { should respond_to :names }
+  it { should respond_to :port }
+  it { should respond_to :port? }
+  it { should respond_to :host }
+  it { should respond_to :configuration }
+  it { should respond_to :configuration= }
+
+  it { should respond_to :FTP_USER }
+  it { should respond_to :FTP_PASSWORD }
+  it { should respond_to :FTP_HOST }
+  it { should respond_to :FTP_DIR }
+
+  its(:names) { should have(5).strings }
+  its(:names) { should include "development" }
+  its(:names) { should include "production" }
+  its(:names) { should include "red" }
+  its(:names) { should include "green" }
+  its(:names) { should include "blue" }
+  its(:available_names) { should have(5).strings }
+  its(:available_names) { should include "development" }
+  its(:available_names) { should include "production" }
+  its(:available_names) { should include "red" }
+  its(:available_names) { should include "green" }
+  its(:available_names) { should include "blue" }
+
+  it "should allow creation of new configuration by name" do
+    ftp = WordpressDeploy::TransferProtocols::Ftp.new "red"
+    ftp.name.should eq "red"
+  end
+
+  it "should only allow configuration names found in the yaml file" do
+    ["production", "development", "red", "green", "blue"].each do |name|
+      subject.name = name
+      subject.name.should eq name
+    end
+    [:production, nil].each do |name|
+      subject.name = name
+      subject.name.should_not eq name
+    end
+  end
+
+  shared_examples "ftp named configuration" do
+    before(:each) { subject.name = name }
+    its(:name)         { should eq name }
+    its(:FTP_USER)     { should eq ftp_user }
+    its(:FTP_PASSWORD) { should eq ftp_password }
+    its(:FTP_HOST)     { should eq ftp_host }
+    its(:FTP_DIR)      { should eq ftp_dir_raw }
+    its(:local_path)   { should eq Environment.wp_dir }
+    its(:remote_path)  { should eq ftp_dir }
+    its(:username)     { should eq ftp_user }
+    its(:password)     { should eq ftp_password }
+    its(:host)         { should eq host }
+    its(:port)         { should eq port }
+    its(:port?)        { should eq has_port }
+  end
+
+  context "development" do
+    it_should_behave_like "ftp named configuration" do
+      let(:name)         { "development" }
+      let(:ftp_user)     { "root" }
+      let(:ftp_password) { "q9&hu6Re_*dReWr_GAba_2wr89#2Ra8$" }
+      let(:ftp_host)     { "localhost" }
+      let(:ftp_dir_raw)  { "/wordpress" }
+      let(:ftp_dir)      { "/wordpress" }
+      let(:host)         { "localhost" }
+      let(:port)         { 21 }
+      let(:has_port)     { false }
+    end
+  end
+
+  context "production" do
+    it_should_behave_like "ftp named configuration" do
+      let(:name)         { "production" }
+      let(:ftp_user)     { "some_user" }
+      let(:ftp_password) { "trecuwawraJaZe6P@kucraDrachustUq" }
+      let(:ftp_host)     { "ftp.abbott.biz:6654" }
+      let(:ftp_dir_raw)  { nil }
+      let(:ftp_dir)      { "/" }
+      let(:host)         { "ftp.abbott.biz" }
+      let(:port)         { 6654 }
+      let(:has_port)     { true }
+    end
+  end
+
+  context "red" do
+    it_should_behave_like "ftp named configuration" do
+      let(:name)         { "red" }
+      let(:ftp_user)     { "red_user" }
+      let(:ftp_password) { "Bun__huPEMeBreM6tebRAp@eguzuQExe" }
+      let(:ftp_host)     { "ftp.hanerutherford.biz" }
+      let(:ftp_dir_raw)  { "/html" }
+      let(:ftp_dir)      { "/html" }
+      let(:host)         { "ftp.hanerutherford.biz" }
+      let(:port)         { 21 }
+      let(:has_port)     { false }
+    end
+  end
+
+  context "green" do
+    it_should_behave_like "ftp named configuration" do
+      let(:name)         { "green" }
+      let(:ftp_user)     { "domenick.dare" }
+      let(:ftp_password) { "Daw&HEWuzaz6sa&epHech_spAKucHaTH" }
+      let(:ftp_host)     { "ftp.yundt.org" }
+      let(:ftp_dir_raw)  { "/wordpress" }
+      let(:ftp_dir)      { "/wordpress" }
+      let(:host)         { "ftp.yundt.org" }
+      let(:port)         { 21 }
+      let(:has_port)     { false }
+    end
+  end
+
+  context "blue" do
+    it_should_behave_like "ftp named configuration" do
+      let(:name)         { "blue" }
+      let(:ftp_user)     { "harrison" }
+      let(:ftp_password) { "w5@reba?9?pepuk7w9a#H86ustaGawE!" }
+      let(:ftp_host)     { "ftp.torphagenes.com" }
+      let(:ftp_dir_raw)  { "/wordpress" }
+      let(:ftp_dir)      { "/wordpress" }
+      let(:host)         { "ftp.torphagenes.com" }
+      let(:port)         { 21 }
+      let(:has_port)     { false }
+    end
+  end
+
+  it "has methods that allow for interactive overwrite" do
+    expect { subject.transmit }.to raise_error(NotImplementedError)
+    expect { subject.receive }.to raise_error(NotImplementedError)
+  end
+
+  it { should respond_to :transmit! }
+  it { should respond_to :receive! }
+
+  context "open FTP connection" do
+    it "with valid credentials" do
+      Net::FTP.should_receive(:new).with("localhost", "root", "q9&hu6Re_*dReWr_GAba_2wr89#2Ra8$")
+      ftp = WordpressDeploy::TransferProtocols::Ftp.new("developer")
+    end
+  end
 
 end
