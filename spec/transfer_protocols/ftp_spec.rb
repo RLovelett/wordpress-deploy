8a540fd5b35ddbed2a0691984454f87d34db0f21
Ryan Lovelett <ryan@lovelett.me>
2012-08-11 22:04:06 -0500

    Test the interaction between Ftp and Net::FTP

--- spec/transfer_protocols/ftp_spec.rb
+++ spec/transfer_protocols/ftp_spec.rb
@@ -152,10 +152,41 @@ describe WordpressDeploy::TransferProtocols::Ftp do
   it { should respond_to :transmit! }
   it { should respond_to :receive! }
 
-  context "open FTP connection" do
+  context "FTP connection" do
+    before(:each) do
+      @ftp = double("ftp")
+      @ftp.should_receive(:connect).with("localhost", 21)
+      @ftp.should_receive(:login).with("root", "q9&hu6Re_*dReWr_GAba_2wr89#2Ra8$")
+      @ftp.should_receive(:passive=).with(true)
+      @ftp.stub(:pwd)
+      @ftp.stub(:closed?).and_return(false)
+      @ftp.stub(:close)
+      @ftp.stub(:chdir)
+      @ftp.stub(:putbinaryfile)
+      Net::FTP.stub(:new).and_return(@ftp)
+      WordpressDeploy::TransferProtocols::Ftp.any_instance.stub(:ftp).and_return(@ftp)
+    end
+
     it "with valid credentials" do
-      Net::FTP.should_receive(:new).with("localhost", "root", "q9&hu6Re_*dReWr_GAba_2wr89#2Ra8$")
       ftp = WordpressDeploy::TransferProtocols::Ftp.new("developer")
+    end
+
+    it "close an open connection" do
+      ftp = WordpressDeploy::TransferProtocols::Ftp.new("developer")
+      ftp.close.should be_true
+      #ftp.close.should be_false
+    end
+
+    it "should send files" do
+      files = Dir.glob(File.join(data_dir, "**/*"))
+
+      Dir.should_receive(:glob).with("/Users/ryan/Source/wordpress-deploy/spec/data/**/*").and_return(files)
+
+      ftp = WordpressDeploy::TransferProtocols::Ftp.new("developer")
+
+      ftp.should_receive(:put_file).exactly(files.count).times
+
+      ftp.transmit!
     end
   end
 
