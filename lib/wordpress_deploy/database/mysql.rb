7b0f679b814350d27ed9e0c0419069a15b90f0cf
Ryan Lovelett <ryan@lovelett.me>
2012-08-18 18:11:45 -0500

    MySql now uses the new DSL

--- lib/wordpress_deploy/database/mysql.rb
+++ lib/wordpress_deploy/database/mysql.rb
@@ -6,6 +6,120 @@ module WordpressDeploy
     class MySql
       include WordpressDeploy::Cli::Helpers
 
+      def initialize
+        @user       ||= "root"
+        @password   ||= ""
+
+        @host       ||= "localhost"
+        @port       ||= 3306
+        @socket     ||= ""
+        @name       ||= "wordpress"
+
+        @has_port   ||= true
+        @has_socket ||= false
+      end
+
+      ##
+      # Name of the database (DB_NAME)
+      def name(new_name = nil)
+        @name = new_name.to_s unless new_name.nil?
+        @name
+      end
+
+      ##
+      # User credentials for the specified database
+      def user(new_user = nil)
+        @user = new_user.to_s unless new_user.nil?
+        @user
+      end
+
+      ##
+      # Password credentials for the specified database
+      def password(new_password = nil)
+        @password = new_password.to_s unless new_password.nil?
+        @password
+      end
+
+      ##
+      # Get just the hostname from DB_HOST. Only different from
+      # DB_HOST if DB_HOST has a socket or a port number in it.
+      def host(new_host = nil)
+        unless new_host.nil?
+          match = /(?<host>.*?)(?=:|\z)(:(?<socket>\/.*)|:(?<port>\d+))?/.match(new_host.to_s)
+          @host = match[:host].to_s unless match[:host].nil?
+
+          # Set the socket information
+          if @has_socket = !match[:socket].nil?
+            @has_socket = true
+            @socket = match[:socket]
+          end
+
+          # Set the port information
+          unless match[:port].nil?
+            @port = match[:port].to_i
+          end
+
+          # Has port is true; unless a socket was set
+          @has_port = !@has_socket
+        end
+
+        # return the host
+        @host
+      end
+
+      ##
+      # This value should be able to be plugged directly into
+      # DB_HOST int he wp-config.php file.
+      def wp_host
+        extra = nil
+        extra = ":#{socket}" if socket?
+        extra = ":#{port}" if port?
+        "#{host}#{extra}"
+      end
+
+      ##
+      # Extract just the port number from the DB_HOST
+      # configuration file. Or return the default port of 3306.
+      def port
+        @port
+      end
+
+      ##
+      # Does DB_HOST contain a port number? (it does if one was
+      # specified and it does not equal 3306; the default MySQL
+      # port number.
+      def port?
+        @has_port && port != 3306
+      end
+
+      ##
+      # Extract just the socket part from the DB_HOST
+      # configuration file. Or return an empty string if none.
+      def socket
+        @socket
+      end
+
+      ##
+      # Does DB_HOST contain a socket path?
+      def socket?
+        @has_socket
+      end
+
+      def charset(new_charset = nil)
+        @charset = new_charset.to_s unless new_charset.nil?
+        @charset
+      end
+
+      def collate(new_collate = nil)
+        @collate = new_collate.to_s unless new_collate.nil?
+        @collate
+      end
+
+      def table_prefix(new_prefix = nil)
+        @prefix = new_prefix.to_s unless new_prefix.nil?
+        @prefix
+      end
+
       def mysqldump
         "#{utility("mysqldump")} #{arguments}"
       end
@@ -13,7 +127,7 @@ module WordpressDeploy
       private
 
       def arguments
-        "-P \"#{db_port}\" -h \"#{db_hostname}\" -u \"#{self.DB_USER}\" -p#{self.DB_PASSWORD} -B \"#{self.DB_NAME}\""
+        "-P \"#{port}\" -h \"#{host}\" -u \"#{user}\" -p#{password} -B \"#{name}\""
       end
 
       ##
--- spec/wordpress_deploy/database/mysql_spec.rb
+++ spec/wordpress_deploy/database/mysql_spec.rb
@@ -13,48 +13,102 @@ describe MySql do
   it { should respond_to :command_name }
   it { should respond_to :mysqldump }
 
-  shared_examples "command based on configuration" do
-    before(:each) do
-      subject.name = name
-      subject.stub(:utility).with("mysqldump").and_return(mysqldump)
-    end
-    its(:arguments) { should eq expected_args }
-    its(:mysqldump) { should eq "#{mysqldump} #{expected_args}" }
-  end
-
-  context "when configuration is 'development'" do
-    it_should_behave_like "command based on configuration" do
-      let(:name) { "development" }
-      let(:expected_args) { "-P \"3306\" -h \"localhost\" -u \"root\" -pq9&hu6Re_*dReWr_GAba_2wr89#2Ra8$ -B \"developer_database_name\"" }
-    end
-  end
-
-  context "when configuration is 'production'" do
-    it_should_behave_like "command based on configuration" do
-      let(:name) { "production" }
-      let(:expected_args) { "-P \"6654\" -h \"abbott.biz\" -u \"some_user\" -ptrecuwawraJaZe6P@kucraDrachustUq -B \"production_database_name\"" }
+  shared_examples "the environment is" do |env_name|
+    before(:all) { WordpressDeploy::Environments.load }
+    subject { WordpressDeploy::Environments.find(env_name).database }
+    context "#{env_name}" do
+      its(:name)     { should eq expected_name }
+      its(:user)     { should eq expected_user }
+      its(:password) { should eq expected_password }
+      its(:host)     { should eq expected_host }
+      its(:wp_host)  { should eq expected_wp_host }
+      its(:charset)  { should eq expected_charset }
+      its(:collate)  { should eq expected_collate }
+      its(:port)     { should eq expected_port }
+      its(:socket)   { should eq expected_socket }
+      its(:port?)    { should eq has_port }
+      its(:socket?)  { should eq has_socket }
     end
   end
 
-  context "when configuration is 'red'" do
-    it_should_behave_like "command based on configuration" do
-      let(:name) { "red" }
-      let(:expected_args) { "-P \"3306\" -h \"hanerutherford.biz\" -u \"red_user\" -pBun__huPEMeBreM6tebRAp@eguzuQExe -B \"red\"" }
-    end
+  it_should_behave_like "the environment is", :development do
+    let(:expected_name)          { "developer_database_name" }
+    let(:expected_user)          { "root" }
+    let(:expected_password)      { "q9&hu6Re_*dReWr_GAba_2wr89#2Ra8$" }
+    let(:expected_host)          { "localhost" }
+    let(:expected_wp_host)       { "localhost" }
+    let(:expected_charset)       { "utf8" }
+    let(:expected_collate)       { "" }
+    let(:expected_port)          { 3306 }
+    let(:expected_socket)        { "" }
+    let(:has_port)               { false }
+    let(:has_socket)             { false }
+
+    let(:expected_args) { "-P \"3306\" -h \"localhost\" -u \"root\" -pq9&hu6Re_*dReWr_GAba_2wr89#2Ra8$ -B \"developer_database_name\"" }
   end
 
-  context "when configuration is 'green'" do
-    it_should_behave_like "command based on configuration" do
-      let(:name) { "green" }
-      let(:expected_args) { "-P \"3306\" -h \"yundt.org\" -u \"domenick.dare\" -pDaw&HEWuzaz6sa&epHech_spAKucHaTH -B \"green\"" }
-    end
+  it_should_behave_like "the environment is", :production do
+    let(:expected_name)          { "production_database_name" }
+    let(:expected_user)          { "some_user" }
+    let(:expected_password)      { "trecuwawraJaZe6P@kucraDrachustUq" }
+    let(:expected_host)          { "abbott.biz" }
+    let(:expected_wp_host)       { "abbott.biz:6654" }
+    let(:expected_charset)       { "utf8" }
+    let(:expected_collate)       { "" }
+    let(:expected_port)          { 6654 }
+    let(:expected_socket)        { "" }
+    let(:has_port)               { true }
+    let(:has_socket)             { false }
+
+    let(:expected_args) { "-P \"6654\" -h \"abbott.biz\" -u \"some_user\" -ptrecuwawraJaZe6P@kucraDrachustUq -B \"production_database_name\"" }
   end
 
-  context "when configuration is 'blue'" do
-    it_should_behave_like "command based on configuration" do
-      let(:name) { "blue" }
-      let(:expected_args) { "-P \"3306\" -h \"torphagenes.com\" -u \"harrison\" -pw5@reba?9?pepuk7w9a#H86ustaGawE! -B \"blue\"" }
-    end
+  it_should_behave_like "the environment is", :red do
+    let(:expected_name)          { "red" }
+    let(:expected_user)          { "red_user" }
+    let(:expected_password)      { "Bun__huPEMeBreM6tebRAp@eguzuQExe" }
+    let(:expected_host)          { "hanerutherford.biz" }
+    let(:expected_wp_host)       { "hanerutherford.biz" }
+    let(:expected_charset)       { "utf8" }
+    let(:expected_collate)       { "" }
+    let(:expected_port)          { 3306 }
+    let(:expected_socket)        { "" }
+    let(:has_port)               { false }
+    let(:has_socket)             { false }
+
+    let(:expected_args) { "-P \"3306\" -h \"hanerutherford.biz\" -u \"red_user\" -pBun__huPEMeBreM6tebRAp@eguzuQExe -B \"red\"" }
+  end
+
+  it_should_behave_like "the environment is", :green do
+    let(:expected_name)          { "green" }
+    let(:expected_user)          { "domenick.dare" }
+    let(:expected_password)      { "Daw&HEWuzaz6sa&epHech_spAKucHaTH" }
+    let(:expected_host)          { "yundt.org" }
+    let(:expected_wp_host)       { "yundt.org" }
+    let(:expected_charset)       { "utf8" }
+    let(:expected_collate)       { "" }
+    let(:expected_port)          { 3306 }
+    let(:expected_socket)        { "" }
+    let(:has_port)               { false }
+    let(:has_socket)             { false }
+
+    let(:expected_args) { "-P \"3306\" -h \"yundt.org\" -u \"domenick.dare\" -pDaw&HEWuzaz6sa&epHech_spAKucHaTH -B \"green\"" }
+  end
+
+  it_should_behave_like "the environment is", :blue do
+    let(:expected_name)          { "blue" }
+    let(:expected_user)          { "harrison" }
+    let(:expected_password)      { "w5@reba?9?pepuk7w9a#H86ustaGawE!" }
+    let(:expected_host)          { "torphagenes.com" }
+    let(:expected_wp_host)       { "torphagenes.com:/tmp/mysql5.sock" }
+    let(:expected_charset)       { "utf8" }
+    let(:expected_collate)       { "" }
+    let(:expected_port)          { 3306 }
+    let(:expected_socket)        { "/tmp/mysql5.sock" }
+    let(:has_port)               { false }
+    let(:has_socket)             { true }
+
+    let(:expected_args) { "-P \"3306\" -h \"torphagenes.com\" -u \"harrison\" -pw5@reba?9?pepuk7w9a#H86ustaGawE! -B \"blue\"" }
   end
 
   context "find commands" do
